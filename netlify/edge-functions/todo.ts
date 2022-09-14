855b804f4726a1a97cbd432097f4cc47578c7625
jaybuidl <jb@kleros.io>
2022-09-15 01:55:39 +0200

    fixed query parameter

--- netlify/edge-functions/todo.ts
+++ netlify/edge-functions/todo.ts
@@ -1,9 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
   console.log(request);
   console.log(context);
-  const { id } = request.queryStringParameters;
+  const id = (new URLPattern(request.headers.get("referer"))).search.split("=")[1];
+  console.log("query: %O", id); 
   const url = Deno.env.get("TODO_BASE_URL");
   const response = await fetch(`${url}/${id}`, {
     "headers": {
